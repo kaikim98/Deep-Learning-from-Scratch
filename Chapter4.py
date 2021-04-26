6b372727701545cb35526fa71e5ff2f439371307
kaikim98 <kaikim98@naver.com>
2021-04-26 19:37:37 +0900

    21.04.26

--- Chapter4.py
+++ Chapter4.py
@@ -117,10 +117,50 @@ gradient_descent(function_2, init_x= init_x, lr=1e-10, step_num=100)
 
 # %% 4.4.2 Gradient in Neural Network
 import sys, os
-sys.path.append(os.pardir)
+os.chdir('E:\\밑바닥부터 시작하는 딥러닝\Deep-Learning-from-Scratch')
 import numpy as np
-# from common.funcitons import softmax, cross_entropy_error
-# from common.gradient import numerical_gradient
+def softmax(x):
+    if x.ndim == 2:
+        x = x.T
+        x = x - np.max(x, axis=0)
+        y = np.exp(x) / np.sum(np.exp(x), axis=0)
+        return y.T 
+
+    x = x - np.max(x) # 오버플로 대책
+    return np.exp(x) / np.sum(np.exp(x))
+
+def cross_entropy_error(y, t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+        
+    # 훈련 데이터가 원-핫 벡터라면 정답 레이블의 인덱스로 반환
+    if t.size == y.size:
+        t = t.argmax(axis=1)
+             
+    batch_size = y.shape[0]
+    return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
+
+
+def numerical_gradient(f, x):
+    h = 1e-4 # 0.0001
+    grad = np.zeros_like(x)
+    
+    it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
+    while not it.finished:
+        idx = it.multi_index
+        tmp_val = x[idx]
+        x[idx] = float(tmp_val) + h
+        fxh1 = f(x) # f(x+h)
+        
+        x[idx] = tmp_val - h 
+        fxh2 = f(x) # f(x-h)
+        grad[idx] = (fxh1 - fxh2) / (2*h)
+        
+        x[idx] = tmp_val # 값 복원
+        it.iternext()   
+        
+    return grad
 
 class simpleNet:
     def __init__(self):
@@ -150,3 +190,105 @@ def f(W):
 
 dW = numerical_gradient(f, net.W)
 print(dW)
+
+# %% 4.5 Implement Learning Algorithm
+# 4.5.1 Implement 2-layer Neural Network class
+
+import sys, os
+sys.path.append(os.pardir)
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))  
+
+def softmax(x):
+    if x.ndim == 2:
+        x = x.T
+        x = x - np.max(x, axis=0)
+        y = np.exp(x) / np.sum(np.exp(x), axis=0)
+        return y.T 
+
+    x = x - np.max(x) # 오버플로 대책
+    return np.exp(x) / np.sum(np.exp(x))
+
+class TwoLayerNet:
+    def __init__(self, input_size, hidden_size, output_size, weight_init_std=0.01):
+        self.params = {}
+        self.params['W1'] = weight_init_std * np.random.randn(input_size, hidden_size)
+        self.params['b1'] = np.zeros(hidden_size)
+        self.params['W2'] = weight_init_std * np.random.randn(hidden_size, output_size)
+        self.params['b2'] = np.zeros(output_size)
+    def predict(self, x):
+        W1, W2 = self.params['W1'], self.params['W2']
+        b1, b2 = self.params['b1'], self.params['b2']
+        
+        a1 = np.dot(x, W1) + b1
+        z1 = sigmoid(a1)
+        a2 = np.dot(z1, W2) + b2
+        y = softmax(a2)
+        
+        return y
+    
+    def loss(self, x, t):
+        y = self.predict(x)
+        
+        return cross_entropy_error(y, t)
+    
+    def accuracy(self, x, t):
+        y = self.predict(x)
+        y = np.argmax(y, axis=1)
+        t = np.argmax(t, axis=1)
+        
+        accuracy = np.sum(y==t) / float(x.shape[0])
+        return accuracy
+    
+    def numerical_gradient(self, x, t):
+        loss_W = lambda W: self.loss(x, t)
+        
+        grads = {}
+        grads['W1'] = numerical_gradient(loss_W, self.params['W1'])
+        grads['b1'] = numerical_gradient(loss_W, self.params['b1'])
+        grads['W2'] = numerical_gradient(loss_W, self.params['W2'])
+        grads['b2'] = numerical_gradient(loss_W, self.params['b2'])
+        
+        return grads
+    
+
+net = TwoLayerNet(input_size=784, hidden_size=100, output_size=10)
+net.params['W1'].shape
+
+x = np.random.rand(100, 784)
+y = net.predict(x)
+
+x = np.random.rand(100, 784)
+t = np.random.rand(100, 10)
+grads = net.numerical_gradient(x, t)
+
+grads['W1'].shape
+
+# %% 4.5.2 Implement Mini-batch learning
+import numpy as np
+import os
+
+os.chdir('E:\\밑바닥부터 시작하는 딥러닝\Deep-Learning-from-Scratch\dataset')
+from mnist import load_mnist
+(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
+
+train_loss_list = []
+
+iters_num = 10000
+train_size = x_train.shape[0]
+batch_size = 100
+learning_rate = 0.1
+network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
+for i in range(iters_num):
+    batch_mas = np.random.choice(train_size, batch_size)
+    x_batch = x_train[batch_mask]
+    t_batch = t_train[batch_mask]
+    
+    grad = network.numerical_gradient(x_batch, t_batch)
+    
+    for key in ('W1', 'b1', 'W2', 'b2'):
+        network.params[key] -= learning_rate * grad[key]
+        
+    loss = network.loss(x_batch, t_batch)
+    train_loss_list.append(loss)
