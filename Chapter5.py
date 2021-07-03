10c23a8291da45b618b7d65535aee32f52733e24
kaikim98 <kaikim98@naver.com>
2021-07-03 19:41:19 +0900

    21.07.03

--- Chapter5.py
+++ Chapter5.py
@@ -122,3 +122,173 @@ class Simgoid:
     
 # %% 5.6 Implement Affine/Softmax Layer
 # 5.6.1 Affine Layer
+X_dot_W = np.array([[0,0,0],[10,10,10]])
+B = np.array([1,2,3])
+
+X_dot_W+B
+dY = np.array([[1,2,3],[4,5,6]])
+dB = np.sum(dY, axis=0)
+
+class Affine:
+    def __init__(self, W, b):
+        self.W = W
+        self.b = b
+        self.x = None
+        self.dW = None
+        self.db = None
+    
+    def forward(self, x):
+        self.x = x
+        out = np.dot(x, self.W) + self.b
+        
+        return out
+    def backward(self, dout):
+        dx = np.dot(dout, self.W.T)
+        self.dW = np.dot(self.W.T, dout)
+        self.db = np.sum(dout, axis=0)
+        
+        return dx
+    
+# %%  5.6.3 Softmax-with-Loss Layer
+class softmaxWithLoss:
+    def __init__(self):
+        self.loss = None
+        self.y = None
+        self.t = None
+        
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+        self.loss = cross_entropy_error(self.y, self.t)
+        return self.loss
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+        dx = (self.y - self.t) / batch_size
+        
+        return dx
+    
+# %% 5.7 Implement Backpropagation
+import sys, os
+sys.path.append(os.pardir)
+import numpy as np
+from common.layers import *
+from common.gradient import numerical_gradient
+from collections import OrderedDict
+class TwoLayerNet:
+    
+    def __init__(self, input_size, hidden_size, ouput_size, weight_init_std=0.01):
+        self.params={}
+        self.params['W1'] = weight_init_std * np.random.randn(input_size, hidden_size)
+        self.params['b1'] = np.zeros(hidden_size)
+        self.params['W2'] = weight_init_std * np.random.randn(hidden_size, ouput_size)
+        self.params['b2'] = np.zeros(output_size)
+        
+        self.layers = OrderedDict()
+        self.layers['Affine1'] = Affine(self.params['W1'], self.params['b1'])
+        self.layers['Relu1'] = Relu()
+        self.layers['Affine2'] = Affine(self.prarms['W2'], self.params['b2'])
+        
+        self.lastLayer = softmaxWithLoss()
+    
+    def predict(self, x):
+        for layer in self.layers.values():
+            x = layer.forward(x)
+            
+        return x
+
+    def loss(self, x, t):
+        y = self.predict(x)
+        return self.lastLayer.forward(y, t)
+    def accuracy(self, x,t):
+        y = self.predict(x)
+        y = np.argmax(y, axis=1)
+        if t.ndim != 1 : t = np.argmax(t, axis=1)
+        
+        accuracy = np.sum(y==t) / float(x.shape[0])
+        return accuracy
+    
+    def numerical_gradient(self, x, t):
+        loss_W = lambda W: self.loss(x, t)
+        
+        grads = {}
+        grad['W1'] = numerical_gradient(loss_W, self.params['W1'])
+        grad['b1'] = numerical_gradient(loss_W, self.params['b1'])
+        grad['W2'] = numerical_gradient(loss_W, self.params['W2'])
+        grad['b2'] = numerical_gradient(loss_W, self.params['b2'])
+        return grads
+    
+    def gradient(self, x, t):
+        self.loss(x,t)
+        
+        dout=1
+        dout = self.lastLayer.backward(dout)
+        
+        layers = list(self.layers.values())
+        layers.reverse()
+        for layer in layers:
+            dout = layer.backward(dout)
+        
+        grads = {}
+        grads['W1'] = self.layers['Affine1'].dW
+        grads['b1'] = self.layers['Affine1'].db
+        grads['W2'] = self.layers['Affine2'].dW
+        grads['W2'] = self.layers['Affine2'].db
+        
+# 5.7.3 Check gradient by BackPropagation
+import sys, os
+sys.path.append(os.pardir)
+import numpy as np
+from dataset.mnist import load_mnist
+
+(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True)
+
+network= TwoLayerNet(input_size=784, hidden_size = 50, ouput_size=10)
+
+x_batch = x_train[:3]
+t_batch = t_train[:3]
+
+grad_numerical = network.numerical_gradient(x_batch, t_batch)
+grad_backprop = network.gradient(x_batch, t_batch)
+
+for key in grad_numerical.keys():
+    diff = np.average(np.abs(grad_backprop[key]- grad_numerical[key]))
+    print(key+':'+str(diff))
+    
+#%% 5.7.4 Implement learning by BackPropagation
+import os, sys
+sys.path.append(pardir)
+import numpy as np
+from dataset.mnist import load_mnist
+
+(x_trian, t_train), (x_test, t_test) = load_mnist(normalize= True, one_hot_level=True)
+network = TwoLayerNet(input_size = 784, hidden_size=50, output_size= 10)
+
+iters_num = 10000
+train_size= x_train.shape[0]
+batch_size = 100
+learning_rate = 0.1
+
+train_lost_list = []
+train_acc_list = []
+test_acc_list= []
+
+iter_per_epoch = max(train_size/batch_size, 1)
+
+for i in range(iters_num):
+    batch_mask = np.random.choice(train_size, batch_size)
+    x_batch = x_train[batch_mask]
+    t_batch = t_train[batch_mask]
+    
+    grad = network.gradient(x_batch, t_batch)
+    
+    for key in ('W1', 'b1', 'W1', 'b2'):
+        network.params[key] -= learning_rate * grad[key]
+    
+    loss = network.loss(x_batch, t_batch)
+    train_loss_list.append(loss)
+    if i%iter_per_epoch ==0:
+        train_acc = network.accuracy(x_train, t_train)
+        test_acc = network.accuracy(x_test, t_test)
+        train_acc_list.append(train_acc)
+        test_acc_list.append(test_acc)
+        print(train_acc, test_acc)
